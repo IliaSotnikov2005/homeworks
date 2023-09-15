989e2c499e661712a059154da76ba1f03261e944
ilia Sotnikov <ilia12st@yandex.ru>
2023-09-16 01:53:23 +0300

    task6 upload

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include "functions.h"
+
+bool isPrime(int number)
+{
+    bool prime = true;
+    for (int divider = 2; divider * divider < number + 1; ++divider)
+    {
+        if (number % divider == 0)
+        {
+            prime = false;
+            break;
+        }
+    }
+    return prime;
+}
 
 int main()
 {
-    return 0;
+    printf("Enter the limit: ");
+    int limit = getNum();
+
+    printf("\nPrime numbers not exceeding a given number: ");
+    for (int number = 2; number < limit + 1; ++number)
+    {
+        if (isPrime(number))
+        {
+            printf("%d ", number);
+        }
+    }
+    printf(limit > 1 ? "\n" : "\n\nThere is no prime numbers not exceeding a given number\n");
 }
