ffa03d0b7e1bbee1ecbcc07216800bce594ed082
ilia Sotnikov <ilia12st@yandex.ru>
2023-09-17 17:15:11 +0300

    task2 upload

--- main.c
+++ main.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
+#include <time.h>
+
+int recursiveExponentiation(int number, int power)
+{
+    if (power == 0)
+    {
+        return 1;
+    }
+    if (power % 2 != 0)
+    {
+        return number * recursiveExponentiation(number, power - 1);
+    }
+    int b = recursiveExponentiation(number, power / 2);
+    return b * b;
+}
+
+int linearExponentiation(int number, int power)
+{
+    int result = 1;
+    for (int i = 0; i < power; ++i)
+    {
+        result *= number;
+    }
+    return result;
+}
 
 int main()
 {
-    return 0;
+    printf("Table of results for numbers from 2 to 7 raised to a power from 0 to 10\n\n\n");
+    for (int number = 2; number < 8; ++number)
+    {
+        printf("Number %d\nLinear    Recursive\n", number);
+        for (int power = 0; power <= 10; ++power)
+        {
+            printf("%9d    %9d\n", linearExponentiation(number, power), recursiveExponentiation(number, power));
+        }
+        printf("\n\n");
+    }
 }
