026094c13a905f640834f3cdbcefc915ea5fc905
ilia Sotnikov <ilia12st@yandex.ru>
2023-09-21 21:43:52 +0300

    task 3 corrected again

--- main.c
+++ main.c
@@ -3,9 +3,15 @@
 #include <math.h>
 #include "functions.h"
 
-int findIncompleteQuotient(int dividend, int divisor)
+int findIncompleteQuotient(int dividend, int divisor, int* exitCode)
 {
-    bool isNegative = (dividend < 0) != (divisor < 0);
+    if (divisor == 0)
+    {
+        exitCode = -1;
+        return 0;
+    }
+    bool dividendNegative = dividend < 0;
+    bool resultNegative = (dividend < 0) != (divisor < 0);
 
     dividend = abs(dividend);
     divisor = abs(divisor);
@@ -16,31 +22,90 @@ int findIncompleteQuotient(int dividend, int divisor)
         dividend -= divisor;
         ++incompleteQuotient;
     }
-    if (isNegative)
+
+    if (dividendNegative && dividend != 0)
     {
-        incompleteQuotient = -incompleteQuotient;
-        if (dividend != 0)
-        {
-            --incompleteQuotient;
-        }
+        ++incompleteQuotient;
     }
-    return incompleteQuotient;
+
+    return resultNegative ? -incompleteQuotient : incompleteQuotient;
+}
+
+int test(void)
+{
+    int exitCode = 0;
+    if (findIncompleteQuotient(-12, 5, &exitCode) != -3)
+    {
+        return 1;
+    }
+    if (findIncompleteQuotient(12, -5, &exitCode) != -2)
+    {
+        return 2;
+    }
+    if (findIncompleteQuotient(12, 5, &exitCode) != 2)
+    {
+        return 3;
+    }
+    if (findIncompleteQuotient(-12, -5, &exitCode) != 3)
+    {
+        return 4;
+    }
+    if (findIncompleteQuotient(-12, 0, &exitCode) != 0)
+    {
+        return 5;
+    }
+    if (findIncompleteQuotient(12, 0, &exitCode) != 0)
+    {
+        return 6;
+    }
+    if (findIncompleteQuotient(12, 12, &exitCode) != 1)
+    {
+        return 7;
+    }
+    if (findIncompleteQuotient(-12, -12, &exitCode) != 1)
+    {
+        return 8;
+    }
+    if (findIncompleteQuotient(12, 1, &exitCode) != 12)
+    {
+        return 9;
+    }
+    if (findIncompleteQuotient(12, -1, &exitCode) != -12)
+    {
+        return 10;
+    }
+    if (findIncompleteQuotient(3, 7, &exitCode) != 0)
+    {
+        return 11;
+    }
+    if (findIncompleteQuotient(-3, 7, &exitCode) != -1)
+    {
+        return 12;
+    }
+    return 0;
 }
 
 int main()
 {
+    if (test() != 0)
+    {
+        printf("failed %d test", test());
+        return -1;
+    }
     printf("Enter the dividend\na = ");
     int number1 = getNum();
 
     printf("\nEnter the divisor\nb = ");
     int number2 = getNum();
 
-    if (number2 == 0)
+    int exitCode = 0;
+    int incompleteQuotient = findIncompleteQuotient(number1, number2, &exitCode);
+
+    if (exitCode == -1)
     {
         printf("\nError: Division by zero\n");
-        return 0;
+        return -1;
     }
 
-    int incompleteQuotient = findIncompleteQuotient(number1, number2);
     printf("\nResult of an incomplete quotient: %d\n", incompleteQuotient);
 }
